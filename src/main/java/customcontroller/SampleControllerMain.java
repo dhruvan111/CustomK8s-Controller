3e0865b150810d139fff97e130d074858ecffe0a
Dhruvan Kadavala <dhruvankadavala111@gmail.com>
2023-07-09 19:28:34 +0530

    K8s client added with shared informer factory

---
+++ src/main/java/customcontroller/SampleControllerMain.java
@@ -0,0 +1,23 @@
+package customcontroller;
+
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
+import io.fabric8.kubernetes.client.informers.SharedInformerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleControllerMain {
+    public static final Logger logger = LoggerFactory.getLogger(SampleControllerMain.class.getSimpleName());
+    public static void main(String[] args) {
+        try (KubernetesClient client = new KubernetesClientBuilder().build()){
+
+            SharedInformerFactory informerFactory = client.informers();
+            SharedIndexInformer<Deployment> deploymentInformer = informerFactory.sharedIndexInformerFor(Deployment.class, 10 * 60 * 1000);
+
+        } catch (Exception e) {
+            logger.info("Exception when interacting with Kubernetes: {} ", e.getMessage());
+        }
+    }
+}
