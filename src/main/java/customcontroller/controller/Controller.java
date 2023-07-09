0db32ae8a4a2fe5231b60a414d46de3fca841357
Dhruvan Kadavala <dhruvankadavala111@gmail.com>
2023-07-09 19:38:16 +0530

    reconcilation logic for deployment added

--- src/main/java/customcontroller/controller/Controller.java
+++ src/main/java/customcontroller/controller/Controller.java
@@ -1,5 +1,7 @@
 package customcontroller.controller;
 
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
@@ -7,6 +9,8 @@ import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -57,7 +61,7 @@ public class Controller {
                     Deployment deployment = eventQueue.take();
                     System.out.println(deployment.getSpec().getReplicas());
                     System.out.println(deployment.getMetadata().getName());
-
+                    reconcileDeployment(deployment);
 
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
@@ -78,6 +82,66 @@ public class Controller {
         }
     }
 
+    // Reconciliation logic for a specific deployment
+    public void reconcileDeployment(Deployment deployment) {
+        String deploymentName = deployment.getMetadata().getName();
+
+        // Get the desired replicas from the deployment
+        int desiredReplicas = deployment.getSpec().getReplicas();
+        System.out.println("Reconciling deployment: " + deploymentName + ", desired replicas: " + desiredReplicas);
+
+        Map<String, Integer> podCountsPerNode = new HashMap<>();
+        // Count the number of pods for this deployment already scheduled on each node
+        PodList podList = client.pods().inNamespace(deployment.getMetadata().getNamespace())
+                .withLabel("app", deploymentName).list();
+        for (Pod pod : podList.getItems()) {
+            String nodeName = pod.getSpec().getNodeName();
+            podCountsPerNode.put(nodeName, podCountsPerNode.getOrDefault(nodeName, 0) - 1);
+        }
+
+        // Iterate over the desired number of replicas and schedule pods accordingly
+        int scheduledPods = 0;
+        for (int i = 0; i < desiredReplicas; i++) {
+            if (scheduledPods >= 3) {
+                // Limit of 3 pods per node reached, stop scheduling
+                System.out.println("Maximum pod limit reached for deployment: " + deploymentName);
+                break;
+            }
+
+            // Find the node with the fewest pods scheduled
+            String targetNode = null;
+            int minPods = Integer.MAX_VALUE;
+            for (Map.Entry<String, Integer> entry : podCountsPerNode.entrySet()) {
+                if (entry.getValue() < minPods) {
+                    minPods = entry.getValue();
+                    targetNode = entry.getKey();
+                }
+            }
+
+            if (targetNode != null) {
+                // Schedule the pod on the selected node
+                System.out.println("Scheduling pod for deployment: " + deploymentName + ", on node: " + targetNode);
+
+                // Create the pod and apply it to the Kubernetes cluster
+                Pod pod = createPod(deploymentName);
+                client.pods().inNamespace(deployment.getMetadata().getNamespace()).createOrReplace(pod);
+
+                // Increment the scheduled pod count for the node
+                podCountsPerNode.put(targetNode, minPods + 1);
+                scheduledPods = minPods+1;
+            }
+        }
+    }
+
+    // Create a sample pod based on the deployment
+    private Pod createPod(String deploymentName) {
+        Pod pod = new Pod();
+        pod.getMetadata().setName("pod-" + deploymentName);
+        pod.getSpec().setNodeName("");
+        // Set the necessary pod spec based on your deployment requirements
+        return pod;
+    }
+
     private void handleAdd(Deployment deployment){
         if ((deployment.getMetadata().getName()).equals("coredns")){
             return;
@@ -92,4 +156,5 @@ public class Controller {
     private void handleDelete(){
 
     }
+
 }
